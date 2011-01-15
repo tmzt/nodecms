b0f7af200e5408fd1b9a8d7726e7f63e299c543d
Timothy Meade <zt.tmzt@gmail.com>
2011-01-15 06:34:04 +0000

    remove some debugging

--- client.js
+++ client.js
@@ -1,13 +1,11 @@
 (function($) {
 	var app = $.sammy('#main', function() {
-		this.get('#/', function(context){alert(this.params.splat);this.partial('/ajax/index');});
-		this.get('#/specific', function(context){alert('specificstuff');});
+		this.get('#/', function(context){this.partial('/ajax/index');});
+		this.get('#/specific', function(context){});
 		this.get('#/:p1/:p2', function(context) {
-			alert(this.params['p1']+'/'+this.params['p2']);
 			this.partial('/ajax/'+this.params['p1']+'/'+this.params['p2']);
 		});
 		this.get('#/:p1', function(context) {
-			alert('p1: ' + this.params['p1']);
 			this.partial('/ajax/'+this.params['p1']);
 		});
 	});
