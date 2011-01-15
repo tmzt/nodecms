4873c2a80676302a1a69879d563e7df669f39c20
Timothy Meade <zt.tmzt@gmail.com>
2011-01-15 06:32:03 +0000

    fix sammy and deliver partials

--- client.js
+++ client.js
@@ -1,9 +1,15 @@
 (function($) {
 	var app = $.sammy('#main', function() {
-		this.get('#', function(context){this.partial('/ajax/index';)});
-		this.get('#:p1/:p2', function(context) {
-			this.partial('/ajax/'+this.params.splat);
-		}); 
+		this.get('#/', function(context){alert(this.params.splat);this.partial('/ajax/index');});
+		this.get('#/specific', function(context){alert('specificstuff');});
+		this.get('#/:p1/:p2', function(context) {
+			alert(this.params['p1']+'/'+this.params['p2']);
+			this.partial('/ajax/'+this.params['p1']+'/'+this.params['p2']);
+		});
+		this.get('#/:p1', function(context) {
+			alert('p1: ' + this.params['p1']);
+			this.partial('/ajax/'+this.params['p1']);
+		});
 	});
-	app.run('#');
+	$(function() { app.run('#/'); });
 })(jQuery);
